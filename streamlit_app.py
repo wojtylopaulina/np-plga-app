6822def401d13e9d05351dd9234f38e26a3373da
wojtylopaulina <wojtylopaulina@gmail.com>
2024-10-14 14:30:59 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.title('PLGA Nanoparticles App ')
 
-st.info("This is app to predict the response variables of PLGA nanoparticles, i.e. particle size and PDI."
+st.info("This is app to predict the response variables of PLGA nanoparticles, i.e. particle size and PDI.")
