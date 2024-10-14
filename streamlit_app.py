5429746f144eceb6a8ea74de1b9f078019915db9
wojtylopaulina <wojtylopaulina@gmail.com>
2024-10-14 14:44:34 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,8 @@ import streamlit as st
 st.title('PLGA Nanoparticles App ')
 
 st.info("This is app to predict the response variables of PLGA nanoparticles, i.e. particle size and PDI.")
+
+
+st.subheader("Choose parameters of microfluidic")
+temp_options = [3, 3.5, 4, 5]
+temp = st.select_slider("Choose Factor A:", options=temp_options)
